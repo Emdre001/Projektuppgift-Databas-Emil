320ac3a8863065436f5703667c57bf64e0c98407
Emdre001 <emil.dressel@gmail.com>
2023-12-10 22:45:44 +0100

    Context added

--- Data/Context.cs
+++ Data/Context.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Projektuppgift_Databas_Emil.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,23 @@ using System.Threading.Tasks;
 
 namespace Projektuppgift_Databas_Emil.Data
 {
-    internal class Context
+    internal class Context : DbContext
     {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<LoanDetail> LoanDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"
+                Server=localhost;
+                Database=NewtonLibrary Emil;
+                Trusted_Connection=True;
+                Trust Server Certificate=Yes;
+                User Id=NewtonLibrary;
+                password=NewtonLibrary");
+
+        }
     }
 }
